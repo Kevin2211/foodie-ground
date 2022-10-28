edc581881dd148104f7e70f744b4af97b7cdfb3a
Kevin Trinh <kevin@Kevins-iMac.local>
2022-10-28 09:11:15 -0500

    fixed marker link bug

--- public/renderMapIndex.js
+++ public/renderMapIndex.js
@@ -18,7 +18,7 @@
             return {
                 lat: latLng.lat,
                 lng: latLng.lng,
-                url: `http://localhost:3000/stores/${store._id}`,
+                url: `https://shrouded-meadow-37611.herokuapp.com/stores/${store._id}`,
                 label: store.title
             }
     })
